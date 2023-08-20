28156dada6753599c81355fa24712553d5b9ff3e
Михаил Ренжин <renzhin@gmail.com>
2023-08-20 21:05:35 +0300

    Вынести в отдельный метод объект заметки. Файл test_logic.py.

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -16,7 +16,7 @@ from notes.models import Note
 User = get_user_model()
 
 
-class TestNote(TestCase):
+class TestLogic(TestCase):
     # Текст комментария понадобится в нескольких местах кода,
     # поэтому запишем его в атрибуты класса.
     NOTE_TITLE = 'Текст заголовка'
@@ -37,6 +37,7 @@ class TestNote(TestCase):
             'text': cls.NOTE_TEXT,
             'slug': cls.NOTE_SLUG
         }
+        cls.note1 = None
 
     def test_anonymous_user_cant_create_note(self):
         # Совершаем запрос от анонимного клиента, в POST-запросе отправляем
@@ -63,21 +64,6 @@ class TestNote(TestCase):
         self.assertEqual(note_for_test.text, self.NOTE_TEXT)
         self.assertEqual(note_for_test.author, self.author1)
 
-    def test_not_unique_slug(self):
-        self.test_note = Note.objects.create(
-            title=self.NOTE_TITLE,
-            text=self.NOTE_TEXT,
-            slug=self.NOTE_SLUG,
-            author=self.author1
-        )
-        response = self.auth_client.post(self.url, data=self.form_data)
-        # Проверяем, что в ответе содержится ошибка формы для поля slug:
-        self.assertFormError(
-            response, 'form', 'slug', errors=(self.test_note.slug + WARNING)
-        )
-        # # Убеждаемся, что количество заметок в базе осталось равным 1:
-        self.assertEqual(Note.objects.count(), 1)
-
     def test_empty_slug(self):
         # Убираем поле slug из словаря:
         self.form_data.pop('slug')
@@ -92,56 +78,58 @@ class TestNote(TestCase):
         # Проверяем, что slug заметки соответствует ожидаемому:
         self.assertEqual(new_note.slug, expected_slug)
 
-    def test_author_can_edit_note(self):
-        self.test_note = Note.objects.create(
-            title='1',
-            text='1',
+    def _create_note1(self):
+        self.note1 = Note.objects.create(
+            title='Тестовый заголовок',
+            text='Содержимое заметки',
+            slug=self.NOTE_SLUG,
             author=self.author1
         )
-        url = reverse('notes:edit', args=(self.test_note.slug,))
+
+    def test_not_unique_slug(self):
+        self._create_note1()
+        response = self.auth_client.post(self.url, data=self.form_data)
+        # Проверяем, что в ответе содержится ошибка формы для поля slug:
+        self.assertFormError(
+            response, 'form', 'slug', errors=(self.note1.slug + WARNING)
+        )
+        # # Убеждаемся, что количество заметок в базе осталось равным 1:
+        self.assertEqual(Note.objects.count(), 1)
+
+    def test_author_can_edit_note(self):
+        self._create_note1()
+        url = reverse('notes:edit', args=(self.note1.slug,))
         response = self.auth_client.post(url, self.form_data)
         self.assertRedirects(response, reverse('notes:success'))
         # Обновляем объект заметки note: получаем обновлённые данные из БД:
-        self.test_note.refresh_from_db()
-        self.assertEqual(self.test_note.title, self.form_data['title'])
-        self.assertEqual(self.test_note.text, self.form_data['text'])
-        self.assertEqual(self.test_note.slug, self.form_data['slug'])
+        self.note1.refresh_from_db()
+        self.assertEqual(self.note1.title, self.form_data['title'])
+        self.assertEqual(self.note1.text, self.form_data['text'])
+        self.assertEqual(self.note1.slug, self.form_data['slug'])
 
     def test_other_user_cant_edit_note(self):
-        self.test_note = Note.objects.create(
-            title='1',
-            text='1',
-            author=self.author1
-        )
-        url = reverse('notes:edit', args=(self.test_note.slug,))
+        self._create_note1()
+        url = reverse('notes:edit', args=(self.note1.slug,))
         self.client.force_login(self.author2)
         response = self.client.post(url, self.form_data)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         # Получаем новый объект запросом из БД.
-        note_from_db = Note.objects.get(id=self.test_note.id)
+        note_from_db = Note.objects.get(id=self.note1.id)
         # Проверяем, что атр. объекта из БД равны атрибутам заметки до запроса.
-        self.assertEqual(self.test_note.title, note_from_db.title)
-        self.assertEqual(self.test_note.text, note_from_db.text)
-        self.assertEqual(self.test_note.slug, note_from_db.slug)
+        self.assertEqual(self.note1.title, note_from_db.title)
+        self.assertEqual(self.note1.text, note_from_db.text)
+        self.assertEqual(self.note1.slug, note_from_db.slug)
 
     def test_author_can_delete_note(self):
-        self.test_note = Note.objects.create(
-            title='1',
-            text='1',
-            author=self.author1
-        )
-        url = reverse('notes:delete', args=(self.test_note.slug,))
+        self._create_note1()
+        url = reverse('notes:delete', args=(self.note1.slug,))
         response = self.auth_client.post(url)
         self.assertRedirects(response, reverse('notes:success'))
         self.assertEqual(Note.objects.count(), 0)
 
     def test_other_user_cant_delete_note(self):
-        self.test_note = Note.objects.create(
-            title='1',
-            text='1',
-            author=self.author1
-        )
-        url = reverse('notes:delete', args=(self.test_note.slug,))
+        self._create_note1()
+        url = reverse('notes:delete', args=(self.note1.slug,))
         self.client.force_login(self.author2)
         response = self.client.post(url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
