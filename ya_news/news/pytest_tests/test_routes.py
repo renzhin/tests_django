b0e03ac2a3b6b2baf2f9df73d83f3c62460d35c9
Михаил Ренжин <renzhin@gmail.com>
2023-08-20 22:47:37 +0300

    Добавить pytest директории + проверку доступности главной страницы и стр. пользователя

---
+++ ya_news/news/pytest_tests/test_routes.py
@@ -0,0 +1,19 @@
+# test_routes.py
+import  pytest
+from http import HTTPStatus
+
+from django.urls import reverse
+
+
+# test_routes.py
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+@pytest.mark.django_db
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
