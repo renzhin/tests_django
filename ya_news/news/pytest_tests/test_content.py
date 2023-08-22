d590645480c85a970e9588844a337b8253acb081
Михаил Ренжин <renzhin@gmail.com>
2023-08-22 09:07:12 +0300

    Изменить порядок методов в соответствие со саиском тестирования в проекте

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -7,13 +7,9 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 def test_max_news_on_home_page(client, create_many_news):
-    # Создайте более 10 новостей в базе данных с помощью фикстуры create_news.
     create_many_news(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-    # Выполните GET-запрос на главную страницу.
     response = client.get(reverse('news:home'))
-    # Проверьте, что статус код ответа равен 200 (OK).
     assert response.status_code == 200
-    # Проверьте, что на странице не более 10 новостей.
     assert len(
         response.context['news_list']
     ) == settings.NEWS_COUNT_ON_HOME_PAGE
@@ -32,20 +28,6 @@ def test_news_order(client):
 
 
 @pytest.mark.django_db
-def test_anonymous_client_has_no_form(client, news):
-    url = reverse('news:detail', args=(news.id,))
-    response = client.get(url)
-    assert 'form' not in response.context
-
-
-@pytest.mark.django_db
-def test_authorized_client_has_form(admin_client, news):
-    url = reverse('news:detail', args=(news.id,))
-    response = admin_client.get(url)
-    assert 'form' in response.context
-
-
-@pytest.mark.django_db
 def test_comments_order(client, create_many_comments, author, news2):
     create_many_comments(2, author, news2)
     url = reverse('news:detail', args=(news2.id,))
@@ -57,3 +39,17 @@ def test_comments_order(client, create_many_comments, author, news2):
     # Проверяем, что время создания первого комментария в списке
     # меньше, чем время создания второго.
     assert all_comments[0].created < all_comments[1].created
+
+
+@pytest.mark.django_db
+def test_anonymous_client_has_no_form(client, news):
+    url = reverse('news:detail', args=(news.id,))
+    response = client.get(url)
+    assert 'form' not in response.context
+
+
+@pytest.mark.django_db
+def test_authorized_client_has_form(admin_client, news):
+    url = reverse('news:detail', args=(news.id,))
+    response = admin_client.get(url)
+    assert 'form' in response.context
